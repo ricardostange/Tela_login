ba93516d20717a00b18e88a1aacce8c2a9e4419e
ricardostange <ricstange@gmail.com>
2022-12-08 13:06:57 -0300

    Passwords are now hashed

--- Cadastro.py
+++ Cadastro.py
@@ -1,20 +1,27 @@
 import PySimpleGUI as sg
+import hashlib
 
-#layout
+def hash_password(password):
+    # Hash a password for storing.
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# Dicionário contendo os usuários e hash das senhas
+logins = dict()
+logins['Cadu'] = 'b7e94be513e96e8c45cd23d162275e5a12ebde9100a425c4ebcdd7fa4dcd897c'
 
 def tela_login():
     sg.theme('Reddit')
     Layout =[
         [sg.Text('Usuario'),sg.Input(key='Usuario')],
-        [sg.Text('Senha'), sg.Input (key='Senha',password_char ='*')],
+        [sg.Text('Senha'), sg.Input (key='Senha', password_char ='*')],
         [sg.Checkbox ('Salvar login?')],
         [sg.Button('Entrar')],
     ]
     return sg.Window('login', layout= Layout, finalize=True)
 
-
 def tela_logado():
     sg.theme('Reddit')
+    save_login(valores['Usuario'], valores['Senha'])
     Layout = [
         [sg.Text('LOGADO')],
         [sg.Button('OK')],
@@ -32,29 +39,29 @@ def tela_recusado():
 
 janela1, janela2, janela3 = tela_login(), None, None
 
-
-def conf_login ():
-    if valores['Usuario'] == 'Cadu' and valores['Senha'] == 'senha':
-        return True
-
-
 while True :
     window, eventos, valores = sg.read_all_windows()
-    if window == janela1 and eventos == sg.WIN_CLOSED:
-        break
-    if window == janela1 and eventos =='Entrar' and conf_login() == True:
-        janela2 = tela_logado()
-        janela1.close()
-    if window == janela2 and eventos == 'OK':
-        janela2.close()
-    if window == janela1 and eventos == 'Entrar' and conf_login() != True:
-        janela1.hide()
-        janela3 = tela_recusado()
-    if window == janela3 and eventos =='Voltar':
-        janela1.un_hide()
-        janela3.close()
-    if window == janela3 and eventos =='Fechar':
-        janela3.close()
+    if window == janela1:
+        if eventos == sg.WIN_CLOSED:
+            break
+        if eventos == 'Entrar' and conf_login() == True:
+            if valores['Salvar login?'] == True:
+                save_login(valores['Usuario'], valores['Senha'])
+            janela2 = tela_logado()
+            janela1.close()
+        if eventos == 'Entrar' and conf_login() != True:
+            janela1.hide()
+            janela3 = tela_recusado()
+    if window == janela2:
+        if eventos == 'OK':
+            janela2.close()
+    if window == janela3:
+        if eventos == 'Voltar':
+            janela1.un_hide()
+            janela3.close()
+        if eventos == 'Fechar':
+            janela3.close()
+
 
 
 
